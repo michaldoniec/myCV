3b322fd69e20495938d42e8043c676dd54d053b8
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-16 17:43:19 +0200

    Create Education model inheriting from PersonBio

---
+++ src/main/java/com/michal/myCV/model/Education.java
@@ -0,0 +1,11 @@
+package com.michal.myCV.model;
+
+
+import java.util.Date;
+
+public class Education extends PersonBio {
+	Education((String name, String description, Date dateFrom, Date dateTo, Integer idUser){
+		super(name, description,dateFrom,dateTo,idUser);
+	}
+
+}
