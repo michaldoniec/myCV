a9d9af8229442759a91faaaeb2ef7f06ad7eff9e
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-16 17:35:41 +0200

    Create Experience model (name, descr, dateFrom, dateTo, idUser)

---
+++ src/main/java/com/michal/myCV/model/Experience.java
@@ -0,0 +1,46 @@
+package com.michal.myCV.model;
+
+import  java.util.Date;
+
+public class Experience extends BaseModel {
+	private String description;
+	private Date dateFrom;
+	private Date dateTo;
+	private Integer idUser;
+
+	public Experience(String name, String description, Date dateFrom, Date dateTo, Integer idUser){
+		super(name);
+		this.description = description;
+		this.dateFrom = dateFrom;
+		this.dateTo = dateTo;
+		this.idUser = idUser;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setDateFrom(Date dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	public void setDateTo(Date dateTo) {
+		this.dateTo = dateTo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Date getDateFrom() {
+		return dateFrom;
+	}
+
+	public Date getDateTo() {
+		return dateTo;
+	}
+
+	public Integer getIdUser() {
+		return idUser;
+	}
+}
