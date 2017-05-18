5b77023ca69b1fb1278f0235c34f4b6f063bcbf8
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-18 20:37:50 +0200

    Refactor space in Education

--- src/main/java/com/michal/myCV/model/Education.java
+++ src/main/java/com/michal/myCV/model/Education.java
@@ -4,6 +4,7 @@ package com.michal.myCV.model;
 import java.time.LocalDate;
 
 public class Education extends PersonBio {
+
 	public Education(String name, String description, LocalDate dateFrom, LocalDate dateTo, Integer idUser){
 		super(name, description,dateFrom,dateTo,idUser);
 	}
