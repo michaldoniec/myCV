e103b947c6cbae5a5b16e7057c905314576ec9da
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-18 20:40:22 +0200

    Create Main class that starts Application

---
+++ src/main/java/com/michal/myCV/Main.java
@@ -0,0 +1,13 @@
+package com.michal.myCV;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try{
+			Application.run();
+		} catch(RuntimeException e){
+			System.out.println("Application stop");
+			Application.getApplicationInstance().stop();
+		}
+	}
+}
