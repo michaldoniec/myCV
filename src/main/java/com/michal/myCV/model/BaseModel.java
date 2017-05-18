92767c1010a392bf6ce385a711e17a86b1c3c14a
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-18 20:37:28 +0200

    Add setName method to BaseModel

--- src/main/java/com/michal/myCV/model/BaseModel.java
+++ src/main/java/com/michal/myCV/model/BaseModel.java
@@ -1,17 +1,19 @@
 package com.michal.myCV.model;
 
-/**
- * Created by michal on 16.05.17.
- */
 public class BaseModel {
 	protected Integer id;
 	protected String name;
+
 	BaseModel(String name){
 		this.name = name;
 	}
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public Integer getId() {
