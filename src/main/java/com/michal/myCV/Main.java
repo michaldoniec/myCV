6138549f9c1a0859c29a1b66b4d1fb8fd7f5175b
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-21 21:10:22 +0200

    Configure port

--- src/main/java/com/michal/myCV/Main.java
+++ src/main/java/com/michal/myCV/Main.java
@@ -1,8 +1,20 @@
 package com.michal.myCV;
 
+import static spark.Spark.port;
+
 public class Main {
 
 	public static void main(String[] args) {
+		port(getHerokuAssignedPort());
 		Application.run();
+			}
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567;
 	}
+
 }
